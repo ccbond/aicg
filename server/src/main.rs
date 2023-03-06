12103a2d454342b747414896817ff2f04706454c
ccbond <1315275049@qq.com>
2023-03-06 20:58:35 +0800

    feat(wechat): varify wechat get request

---
+++ server/src/main.rs
@@ -0,0 +1,47 @@
+#[macro_use]
+extern crate rocket;
+use rocket::form::Form;
+use sha1::{Digest, Sha1};
+use std::env;
+
+#[derive(FromForm)]
+struct WechatVarify<'r> {
+    signature: &'r str,
+    timestamp: &'r str,
+    nonce: &'r str,
+    echostr: &'r str,
+}
+
+#[get("/", data = "<wechat_varify>")]
+fn wechat_varify(wechat_varify: Form<WechatVarify<'_>>) -> u32 {
+    let args: Vec<String> = env::args().collect();
+
+    let signature = wechat_varify.signature;
+    let token_key = "token";
+    let token = env::var("TOKEN")
+        .expect("Failed to read TOKEN from environment variable")
+        .as_str();
+    let timestamp = wechat_varify.timestamp;
+    let nonce = wechat_varify.nonce;
+    let mut tmp_array = vec![token, timestamp, nonce];
+    tmp_array.sort();
+    let tmp_str = tmp_array.join("");
+
+    let mut hasher = Sha1::new();
+    hasher.update(tmp_str.as_bytes());
+
+    let hash_code = hasher.finalize();
+    let hash_encode = hex::encode(hash_code);
+    let hash_str = String::from_utf8(hash_encode.into()).unwrap().as_str();
+
+    if hash_str == signature {
+        return wechat_varify.echostr.parse::<u32>().unwrap();
+    } else {
+        return 0;
+    }
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![wechat_varify])
+}
